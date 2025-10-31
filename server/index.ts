921b835ebd9223b48733f47de4b720a2adaebe8c
ANEESH <aneesh06042006@gmail.com>
2025-10-31 15:57:36 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import fs from "fs";
-import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 
